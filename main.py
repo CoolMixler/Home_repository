fcb64fe8d8541ecc0ec8e1c381cf2ebb13f7b423
CoolAlex21 <was.mos@mail.ru>
2021-05-04 18:29:06 +0300

    Version 1.0

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May  4 17:57:52 2021
-
-@author: alexander1
-"""
+from source import Source
 print('Hello world')
+s = Source()
+print(s.msg)
+print("it is V2.0")
 
